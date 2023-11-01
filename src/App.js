0a99cc157a3401df7dcf191f2ef6f8059bcf1450
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-11-02 00:37:53 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Cart } from './components/cart/Cart';
+import { FilterPanel } from './components/filterPanel/FilterPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+      <FilterPanel />
+    </div>
+  );
+}
+
+export default App;
+
